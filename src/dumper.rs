59c4b387f6eb773149f48dd32efa59cdaaaf893c
Simon Marechal <simon.marechal@synacktiv.com>
2023-08-18 09:30:54 +0200

    chore: format

--- src/dumper.rs
+++ src/dumper.rs
@@ -13,86 +13,86 @@ lazy_static::lazy_static! {
 
 #[derive(Debug)]
 enum Mode {
-  DumpRules,
-  ApplyRules,
+    DumpRules,
+    ApplyRules,
 }
 
 impl std::str::FromStr for Mode {
-  type Err = &'static str;
-  fn from_str(s: &str) -> Result<Self, Self::Err> {
-    match s {
-      "dump" => Ok(Mode::DumpRules),
-      "apply" => Ok(Mode::ApplyRules),
-      _ => Err("Accepted modes are dump and apply"),
+    type Err = &'static str;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "dump" => Ok(Mode::DumpRules),
+            "apply" => Ok(Mode::ApplyRules),
+            _ => Err("Accepted modes are dump and apply"),
+        }
     }
-  }
 }
 
 #[derive(Debug, StructOpt)]
 struct Opts {
-  /// Execution mode
-  #[structopt(long = "mode", default_value("dump"))]
-  mode: Mode,
-  /// Only use rules that work in Hashcat
-  #[structopt(long = "hashcat")]
-  hashcat: bool,
-  /// Dictionary to apply rules on
-  #[structopt(long = "dict")]
-  dict: Option<String>,
+    /// Execution mode
+    #[structopt(long = "mode", default_value("dump"))]
+    mode: Mode,
+    /// Only use rules that work in Hashcat
+    #[structopt(long = "hashcat")]
+    hashcat: bool,
+    /// Dictionary to apply rules on
+    #[structopt(long = "dict")]
+    dict: Option<String>,
 }
 
 fn main() {
-  let opt = Opts::from_args();
-  match opt.mode {
-    Mode::DumpRules => {
-      if opt.hashcat {
-        for r in genmutate()
-          .into_iter()
-          .filter(|rs| rs.iter().all(rules::hashcat_rule))
-        {
-          println!(
-            "{}",
-            rules::show_rules(&r, true).expect("invalid rule got printed :(")
-          );
-        }
-      } else {
-        println!("[List.Rules:RulesFinderRaw]");
-        for r in genmutate()
-          .into_iter()
-          .filter(|rs| rs.iter().all(rules::hashcat_rule))
-        {
-          match rules::show_rules(&r, false) {
-            Some(s) => println!("{}", s),
-            None => {
-              println!("!! hashcat logic ON");
-              println!(
-                "{}",
-                rules::show_rules(&r, true).expect("invalid rule got printed")
-              );
-              println!("!! hashcat logic OFF");
+    let opt = Opts::from_args();
+    match opt.mode {
+        Mode::DumpRules => {
+            if opt.hashcat {
+                for r in genmutate()
+                    .into_iter()
+                    .filter(|rs| rs.iter().all(rules::hashcat_rule))
+                {
+                    println!(
+                        "{}",
+                        rules::show_rules(&r, true).expect("invalid rule got printed :(")
+                    );
+                }
+            } else {
+                println!("[List.Rules:RulesFinderRaw]");
+                for r in genmutate()
+                    .into_iter()
+                    .filter(|rs| rs.iter().all(rules::hashcat_rule))
+                {
+                    match rules::show_rules(&r, false) {
+                        Some(s) => println!("{}", s),
+                        None => {
+                            println!("!! hashcat logic ON");
+                            println!(
+                                "{}",
+                                rules::show_rules(&r, true).expect("invalid rule got printed")
+                            );
+                            println!("!! hashcat logic OFF");
+                        }
+                    }
+                }
             }
-          }
         }
-      }
-    }
-    Mode::ApplyRules => {
-      let rdict = opt.dict.expect("Missing --dict parameter");
-      let file = File::open(rdict).unwrap();
-      let reader = BufReader::new(file);
-      let rlns: Result<Vec<String>, _> = reader.lines().collect();
-      let lns = rlns.unwrap();
-      for r in genmutate()
-        .into_iter()
-        .filter(|rs| rs.iter().all(rules::hashcat_rule))
-      {
-        // println!("// {:?}", r);
-        for l in &lns {
-          if let Some(o) = mutate(l.as_bytes(), &r) {
-            std::io::stdout().write_all(&o).unwrap();
-            std::io::stdout().write_all(b"\n").unwrap();
-          }
+        Mode::ApplyRules => {
+            let rdict = opt.dict.expect("Missing --dict parameter");
+            let file = File::open(rdict).unwrap();
+            let reader = BufReader::new(file);
+            let rlns: Result<Vec<String>, _> = reader.lines().collect();
+            let lns = rlns.unwrap();
+            for r in genmutate()
+                .into_iter()
+                .filter(|rs| rs.iter().all(rules::hashcat_rule))
+            {
+                // println!("// {:?}", r);
+                for l in &lns {
+                    if let Some(o) = mutate(l.as_bytes(), &r) {
+                        std::io::stdout().write_all(&o).unwrap();
+                        std::io::stdout().write_all(b"\n").unwrap();
+                    }
+                }
+            }
         }
-      }
     }
-  }
 }
